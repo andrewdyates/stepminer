5d6c6825ef8aab8a89ccb3cc6cfab0edb014c829
Andrew Yates <andrewyates.name@gmail.com>
2012-11-15 17:18:36 -0500

    working stepminer threshold script

--- __init__.py
+++ __init__.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python
-"""
-python script.py outdir=$HOME/Desktop fname=$HOME/Desktop/sahootest/mRNA10.tab
-"""
 from matrix_io import *
 import subprocess
 
 
-CMD = "java -jar %s -t OneStep %%s -o %%s" % (os.path.abspath("stepminer-1.1.jar"))
+CMD = "time java -jar %s -t OneStep %%s -o %%s" % (os.path.abspath("stepminer-1.1.jar"))
 
-def stepminer_fname(fname, outdir):
-  return os.path.join(outdir, os.path.basename(fname) + ".stepmine.pcl")
+def get_stepminer_in_fname(fname):
+  return fname + ".stepmine.pcl"
 
-def stepminer_out_fname(fname, outdir):
-  return os.path.join(outdir, os.path.basename(fname) + ".stepmine.pcl.ann")
+def get_stepminer_out_fname(fname):
+  return fname + ".ann"
+
+def get_thresh_out_fname(fname):
+  return fname + ".stepmine_thresh.txt"
 
 def format_for_stepminer(fname, outdir):
-  """Save a copy of an expression matrix for stepminer."""
+  """Save a copy of an expression matrix for stepminer, return loaded, sorted matrix."""
   D = load(fname)
   for i in xrange(D['M'].shape[0]):
     D['M'][i] = sorted(D['M'][i])
-  fp = open(stepminer_fname(fname, outdir), 'w')
+  stepminer_in_fpath = os.path.join(outdir, get_stepminer_in_fname(os.path.basename(fname)))
+  fp = open(stepminer_in_fpath, 'w')
   fp.write('YORF\tNAME\tGWEIGHT\t')
   fp.write("\t".join(map(str,xrange(D['M'].shape[1]))))
   fp.write('\nEWEIGHT\t\t\t')
@@ -28,18 +29,33 @@ def format_for_stepminer(fname, outdir):
   hacked_row_ids = ["%s\t%s\t1" % (s,s) for s in D['row_ids']]
   save(D['M'], fp, ftype='txt', row_ids=hacked_row_ids)
   fp.close()
-  return stepminer_fname(fname, outdir)
+  return stepminer_in_fpath, D
 
-def run_stepminer(fname, outdir):
-  in_fname = format_for_stepminer(fname, outdir)
-  out_fname = stepminer_out_fname(fname, outdir)
-  cmd = CMD % (in_fname, out_fname)
-  print cmd
+def run_stepminer(in_fpath, verbose=True):
+  out_fpath = get_stepminer_out_fname(in_fpath)
+  cmd = CMD % (in_fpath, out_fpath)
+  if verbose:
+    print cmd
   subprocess.call(cmd, shell=True)
-  fp = open(stepminer_out_fname(fname, outdir))
-  # TODO: extract rowid, p-value, threshold idx
-  # TODO: save p-value, row_id, threshold idx, threshold value in original row order
-  fp.next()
+  return out_fpath
+
+def transform_output(fp, row_ids, M):
+  """Parse p-value, row_id, threshold idx, from .ann stepminer output file;
+       Return threshold value in original row order.
+  Args:
+    fp: [*str] open file pointer to stepminer .ann results
+    row_ids: [str] of row IDs in original matrix order
+    M: np.array of per-row column-sorted original matrix; rows in original order
+  Returns:
+    obj?
+  """
+  fp.next() # skip first line
+  rows = {}
+  rowid_idx = dict([(s, i) for i, s in enumerate(row_ids)])
   for i, line in enumerate(fp):
     row = line.split('\t')
-    rowid, pv, t = row[0], row[4], row[8]
+    rowid, t = row[0], row[8]
+    # associate row ID with threshold value
+    rows[rowid] = M[rowid_idx[rowid],int(t)]
+  return rows
+    
--- script.py
+++ script.py
@@ -1,9 +1,35 @@
 #!/usr/bin/python
+"""Handle to call stepminer to find midpoint thresholds.
+
+python script.py outdir=$HOME/Desktop fname=example/mRNA10.tab
+"""
 from __init__ import *
 import sys
 
-def main(fname, outdir=""):
-  run_stepminer(fname, outdir)
+def main(fname=None, outdir="", cleanup=False):
+  assert fname
+  print "Formatting .tab input matrix %s for stepminer input." % (fname)
+  stepmine_in_fname, D = format_for_stepminer(fname, outdir)
+  print "Loaded and sorted .tab matrix (%s by %s)" % D['M'].shape
+  print "Created stepminer input file %s." % (stepmine_in_fname)
+  print "Running stepminer..."
+  stepmine_out_fname = run_stepminer(stepmine_in_fname, verbose=True)
+  print "Stepminer results saved in file %s." % (stepmine_out_fname)
+  print "Parsing stepminer results..."
+  row_thresholds = transform_output(open(stepmine_out_fname), D['row_ids'], D['M'])
+  thresh_out_fpath = os.path.join(outdir, get_thresh_out_fname(os.path.basename(fname)))
+  print "Saving row thresholds in input matrix row order in file %s" % (thresh_out_fpath)
+  fp = open(thresh_out_fpath, 'w')
+  for rowid in D['row_ids']:
+    fp.write("%s\t%f\n" % (rowid, row_thresholds[rowid]))
+  fp.close()
+  print "Saved %s." % (thresh_out_fpath)
+  if cleanup:
+    print "Deleting intermediate files..."
+    os.remove(stepmine_in_fname); print "Deleted", stepmine_in_fname
+    os.remove(stepmine_out_fname); print "Deleted", stepmine_out_fname
+  print "Script complete."
+  
 
 if __name__ == "__main__":
   args = dict([s.split('=') for s in sys.argv[1:]])
